bf3beeead52863a67af424ae45a6927be5f4864e
Siona Vivian <SionaSVivian@outlook.com>
2021-05-09 07:57:51 -0700

    Initial commit

---
+++ README.rb
@@ -0,0 +1,14 @@
+Basic program that logs time per day
+
+logger [none/MINS] Update latest record created that day or create one if MINS present,
+view all records if no MINS present
+
+       -n [MINS] Create new record for the day
+
+       -u [none/DATE] Update last record or record at DATE(DD-MM-YY)
+
+       =v View all records
+
+       -d [DATE] Deletes all records at DATE(DD-MM-YY)
+
+       -s (none/-day/-week/-month/-year) (none/-mins/-hours) Total time for a period of time
