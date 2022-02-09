b777ce169d0e21a37cfb17dbf15b43463b636d84
Siona Vivian <SionaSVivian@outlook.com>
2022-02-08 20:24:53 -0800

    TimeLogger: rewrite startup logic

--- lib/time_logger.rb
+++ lib/time_logger.rb
@@ -1,32 +1,27 @@
-require 'yaml'
+require 'sqlite3'
 require 'date'
 
 class Logger
-  def initialize(filename="time_log", format="%d-%m-%y")
-    @filename = filename
-    @format = "%d-%m-%y"
-    File.new(@filename, "w") if !File.file? @filename
+  attr_reader :connection
+
+  def initialize(kwargs = { filename: 'time_log.db', format: '%d-%m-%y' })
+    @connection = SQLite3::Database.new(kwargs[:filename])
+    @format = kwargs[:format]
+    setup_database
+    self
   end
-  def create(mins=0, date=today)
-    open_file('a') do |file|
-      file.write({date => mins}.to_yaml)
-    end
-    if date != today
-      lines = get_lines
-      lines.map! {|line| YAML.load(line)}
-      lines.sort! {|a, b| parse_date(a.keys[0]) <=> parse_date(b.keys[0])}
-      save lines.map {|line| line.to_yaml}
-    end
-    {date => mins}
+  def add(mins = 0, date = today)
+    nil
   end
-  def update(mins, date=today)
+  def update(mins, date = today)
     return create(mins, date) if !record?(date)
     lines = get_lines.reverse
     new_record = nil
     lines.map! do |line|
       record = YAML.load(line)
       if record.keys[0] == date
-        new_record = ({record.keys[0] => record[record.keys[0]] + mins}).to_yaml
+        new_record =
+          ({ record.keys[0] => record[record.keys[0]] + mins }).to_yaml
       else
         line
       end
@@ -35,26 +30,26 @@ class Logger
     YAML.load(new_record)
   end
   def all
-    get_lines.map{ |line| YAML.load(line) }
+    get_lines.map { |line| YAML.load(line) }
   end
-  def delete(date=today)
+  def delete(date = today)
     lines = get_lines
-    lines.delete_if {|record| YAML.load(record).keys[0] == date}
+    lines.delete_if { |record| YAML.load(record).keys[0] == date }
     save lines
     nil
   end
-  def sum(option="week")
+  def sum(option = 'week')
     total_sum = 0
     end_date =
-    if option == "month"
-      month_start
-    elsif option == "year"
-      parse_date(today).prev_year
-    elsif option == "day"
-      parse_date(today) - 1
-    else
-      parse_date(today) - 7
-    end
+      if option == 'month'
+        month_start
+      elsif option == 'year'
+        parse_date(today).prev_year
+      elsif option == 'day'
+        parse_date(today) - 1
+      else
+        parse_date(today) - 7
+      end
     get_lines.each do |line|
       record = YAML.load(line)
       total_sum += record.values[0] if parse_date(record.keys[0]) >= end_date
@@ -64,21 +59,24 @@ class Logger
   def find(date)
     open_file('r') do |file|
       last_date = nil
-      file.readlines.reverse.each do |line|
-        record = YAML.load(line)
-        return nil if record.nil?
-        parsed_date = parse_date(record.keys[0])
-        return record if date == parsed_date.strftime(@format)
-        return nil if !last_date.nil? && parsed_date > last_date
-        last_date = parsed_date
-      end
+      file
+        .readlines
+        .reverse
+        .each do |line|
+          record = YAML.load(line)
+          return nil if record.nil?
+          parsed_date = parse_date(record.keys[0])
+          return record if date == parsed_date.strftime(@format)
+          return nil if !last_date.nil? && parsed_date > last_date
+          last_date = parsed_date
+        end
     end
     nil
   end
-  def record?(date=today)
+  def record?(date = today)
     !find(date).nil?
   end
-  def average(month=DateTime.now.month)
+  def average(month = DateTime.now.month)
     count = 0
     total = 0
     get_lines.reverse.each do |line|
@@ -95,7 +93,43 @@ class Logger
 
   private
 
-  def open_file(mode="r")
+  def create(table, attributes)
+    sql = <<~EOS
+      INSERT INTO #{table}(#{attributes.keys.join('')})
+      VALUES (#{'?' * attributes.length})
+      EOS
+  end
+  def setup_database
+    tables = { Logs: <<~EOS, LogTypes: <<~EOS }
+      CREATE TABLE Logs
+        (id integer primary key,
+         date datetime default current_timestamp,
+         log_type integer
+        )
+      EOS
+      CREATE TABLE LogTypes
+        (id integer primary key,
+         name string,
+        )
+      EOS
+
+    tables.each do |table_name, sql|
+      if !table?(table_name)
+        puts "Created table: #{table_name}"
+        @connection.execute(sql)
+      end
+    end
+  end
+  def table?(name)
+    sql = <<~EOS
+        SELECT count(name)
+        FROM sqlite_master
+        WHERE type='table'
+        AND name='#{name}'
+      EOS
+    @connection.execute(sql) != 0
+  end
+  def open_file(mode = 'r')
     file = File.open(@filename, mode)
     yield file
     file.close
@@ -103,12 +137,12 @@ class Logger
   def get_lines
     lines = []
     open_file do |file|
-      file.readlines.each { |line| lines << line unless line == "---\n"}
+      file.readlines.each { |line| lines << line unless line == "---\n" }
     end
-    lines.keep_if{|entry| !entry.nil?}
+    lines.keep_if { |entry| !entry.nil? }
   end
   def save(array)
-    open_file("w") {|file| file.puts array }
+    open_file('w') { |file| file.puts array }
   end
   def parse_date(date)
     DateTime.parse(Date.strptime(date, @format).to_s)
@@ -116,7 +150,7 @@ class Logger
   def today
     DateTime.now.strftime(@format)
   end
-  def month_start(date=DateTime.now)
+  def month_start(date = DateTime.now)
     parse_date(date.strftime('01-%m-%Y'))
   end
 end
