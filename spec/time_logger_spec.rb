4ae9c986ab00235208581b8fcaee7343e5e0a723
Siona Vivian <SionaSVivian@outlook.com>
2022-02-08 20:25:11 -0800

    TimeLoggerSpec: add initial tests

--- spec/time_logger_spec.rb
+++ spec/time_logger_spec.rb
@@ -1,10 +1,34 @@
-require './lib/time_logger.rb'
+require './lib/time_logger'
+require 'date'
 
 RSpec.configure do |c|
   c.example_status_persistence_file_path = 'spec/examples.txt'
 end
 
 RSpec.describe 'Logger' do
-  it 'returns something' do
+  time_log = Logger.new({ filename: ':memory:' })
+  count_records = ->(name) { time_log.connection.execute(<<~EOS) }
+    SELECT count(*)
+    FROM #{name}
+  EOS
+
+  has_table = ->(name) { time_log.connection.execute(<<~EOS) != 0 }
+    SELECT count(name)
+    FROM sqlite_master
+    WHERE type='table'
+    AND name='#{name}'
+  EOS
+
+  it 'creates database' do
+    expect(time_log.connection.nil?).to eq(false)
+  end
+  it 'creates Log and LogTypes tables' do
+    expect(has_table.('Logs')).to eq(true)
+    expect(has_table.('LogTypes')).to eq(true)
+  end
+  it 'creates Log records and appropriate LogType entry' do
+    time_log.add({ log_type: 'dog_walking', mins: 0 })
+    expect(count_records('Logs')).to eq(1)
+    expect(count_records('LogTypess')).to eq(1)
   end
 end
