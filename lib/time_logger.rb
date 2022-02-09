2634ed3dbb6bc0a3ee9ad717a3d8c6cfee2ca746
Siona Vivian <SionaSVivian@outlook.com>
2022-02-08 20:24:53 -0800

    TimeLogger: rewrite to use SQl, add support for multiple log types

--- lib/time_logger.rb
+++ lib/time_logger.rb
@@ -1,122 +1,259 @@
-require 'yaml'
+require 'sqlite3'
 require 'date'
+require 'time'
 
 class Logger
-  def initialize(filename="time_log", format="%d-%m-%y")
-    @filename = filename
-    @format = "%d-%m-%y"
-    File.new(@filename, "w") if !File.file? @filename
+  attr_reader :connection
+
+  def initialize(filename: 'time_log.db', output_format: '%d-%m-%y')
+    @connection = SQLite3::Database.new(filename)
+    @format = output_format
+    setup_database
+    self
   end
-  def create(mins=0, date=today)
-    open_file('a') do |file|
-      file.write({date => mins}.to_yaml)
-    end
-    if date != today
-      lines = get_lines
-      lines.map! {|line| YAML.load(line)}
-      lines.sort! {|a, b| parse_date(a.keys[0]) <=> parse_date(b.keys[0])}
-      save lines.map {|line| line.to_yaml}
-    end
-    {date => mins}
-  end
-  def update(mins, date=today)
-    return create(mins, date) if !record?(date)
-    lines = get_lines.reverse
-    new_record = nil
-    lines.map! do |line|
-      record = YAML.load(line)
-      if record.keys[0] == date
-        new_record = ({record.keys[0] => record[record.keys[0]] + mins}).to_yaml
-      else
-        line
-      end
-    end
-    save lines.reverse
-    YAML.load(new_record)
-  end
-  def all
-    get_lines.map{ |line| YAML.load(line) }
-  end
-  def delete(date=today)
-    lines = get_lines
-    lines.delete_if {|record| YAML.load(record).keys[0] == date}
-    save lines
-    nil
-  end
-  def sum(option="week")
-    total_sum = 0
-    end_date =
-    if option == "month"
-      month_start
-    elsif option == "year"
-      parse_date(today).prev_year
-    elsif option == "day"
-      parse_date(today) - 1
+  def add_or_update(mins: 0, date: today, log_type: '')
+    formatted_date = date.class == String ? format_date(date) : date.to_s
+    if !exists?(
+         table: 'Logs',
+         values: {
+           date: formatted_date,
+           log_type: log_type,
+         },
+       )
+      @connection.execute(<<~EOS)
+        INSERT INTO Logs (date, minutes, log_type)
+        VALUES ('#{formatted_date}', #{mins}, '#{log_type}')
+      EOS
     else
-      parse_date(today) - 7
+      update(
+        table: 'Logs',
+        select_column: {
+          date: formatted_date,
+          log_type: log_type,
+        },
+        update_columns: {
+          minutes: "minutes + #{mins}",
+        },
+      )
     end
-    get_lines.each do |line|
-      record = YAML.load(line)
-      total_sum += record.values[0] if parse_date(record.keys[0]) >= end_date
+    if !exists?(table: 'LogTypes', values: { name: log_type })
+      @connection.execute("INSERT INTO LogTypes (name) VALUES ('#{log_type}')")
     end
-    total_sum
+    self
   end
-  def find(date)
-    open_file('r') do |file|
-      last_date = nil
-      file.readlines.reverse.each do |line|
-        record = YAML.load(line)
-        return nil if record.nil?
-        parsed_date = parse_date(record.keys[0])
-        return record if date == parsed_date.strftime(@format)
-        return nil if !last_date.nil? && parsed_date > last_date
-        last_date = parsed_date
+  def all(display: '', width: 24, row_size: 3)
+    if display != 'LogTypes'
+      records =
+        @connection.execute(
+          'SELECT date, minutes FROM Logs WHERE log_type=? ORDER BY date ASC',
+          [display],
+        )
+      records =
+        records
+          .each_with_index
+          .map do |data, i|
+            date_segments = data[0].split('-').reverse
+            "#{[*date_segments[0..-2], date_segments[-1][-2..-1]].join('-')}: #{data[1]}"
+              .center(width) + (((i + 1) % row_size) == 0 ? "\n" : ' | ')
+          end
+          .join('')
+      return records
+    end
+    records = @connection.execute('SELECT name FROM LogTypes ORDER BY id ASC')
+    records
+      .each_with_index
+      .map do |name, i|
+        name[0].center(width) + (((i + 1) % row_size) == 0 ? "\n" : ' | ')
       end
-    end
-    nil
+      .join('')
   end
-  def record?(date=today)
-    !find(date).nil?
+  def delete(table: '', remove: {})
+    sql = "DELETE FROM #{table} WHERE #{remove.to_a[0][0]}=?"
+    @connection.execute(sql, [remove.to_a[0][1]])
   end
-  def average(month=DateTime.now.month)
-    count = 0
-    total = 0
-    get_lines.reverse.each do |line|
-      record = YAML.load(line)
-      record_month = parse_date(record.keys[0]).month
-      break if record_month < month
-      if record_month == month
-        total += record.values[0]
-        count += 1.0
-      end
+  def sum(log_type: '', option: '', start: nil)
+    aggregate(
+      aggregate_function: 'sum',
+      log_type: log_type,
+      option: option,
+      start: start,
+    )
+  end
+  def average(log_type: '', option: '', start: nil)
+    aggregate(
+      aggregate_function: 'avg',
+      log_type: log_type,
+      option: option,
+      start: start,
+    )
+  end
+  def import_from_yaml(log_type: '', filename: 'time_log')
+    get_lines(filename).each do |entry|
+      date = entry.match(/(?<='|)[\d-]+(?=':|:)/).to_s
+      minutes = entry.match(/(?<=: )\d+(?=\n)/).to_s
+      add_or_update(log_type: log_type, date: parse_date(date), mins: minutes)
     end
-    count != 0 ? total / count : 0
+  end
+  def set_user_preference(name: '', new_val: '')
+    update(
+      table: 'UserPreferences',
+      select_column: {
+        name: name,
+      },
+      update_columns: {
+        val: new_val,
+      },
+    )
+  end
+  def get_user_preference(name: '')
+    result =
+      @connection.execute('SELECT * from UserPreferences WHERE name=?', [name])[
+        0
+      ]
+    return result if name != 'default_log_type'
+    if result[1].length == 0
+      @connection.execute('SELECT name FROM LogTypes LIMIT 1')
+    else
+      result
+    end
   end
 
   private
 
-  def open_file(mode="r")
-    file = File.open(@filename, mode)
-    yield file
-    file.close
+  def create(table, attributes)
+    sql = <<~EOS
+      INSERT INTO #{table}(#{attributes.keys.join('')})
+      VALUES (#{'?' * attributes.length})
+      EOS
   end
-  def get_lines
-    lines = []
-    open_file do |file|
-      file.readlines.each { |line| lines << line unless line == "---\n"}
+  def setup_database
+    tables = { Logs: <<~EOS, LogTypes: <<~EOS, UserPreferences: <<~EOS }
+      CREATE TABLE Logs
+        (id integer primary key,
+         date datetime default current_timestamp,
+         minutes integer,
+         log_type string
+        )
+      EOS
+      CREATE TABLE LogTypes
+        (id integer primary key,
+         name string
+        )
+      EOS
+      CREATE TABLE UserPreferences
+        (id integer primary key,
+         name string,
+         val string
+        );
+      INSERT INTO UserPreferences (name, val)
+      VALUES (row_size, 3);
+      INSERT INTO UserPreferences (name, val)
+      VALUES (width, 24);
+      INSERT INTO UserPreferences (name, val)
+      VALUES (default_log_type, "");
+      INSERT INTO UserPreferences (name, val)
+      VALUES (time_unit, minutes);
+      INSERT INTO UserPreferences (name, val)
+      VALUES (kanji, false);
+      INSERT INTO UserPreferences (name, val)
+      VALUES (hiragana, false);
+      EOS
+
+    tables.each do |table_name, sql|
+      if !table?(table_name)
+        @connection.execute(sql)
+        puts "Created table: #{table_name}"
+      end
     end
-    lines.keep_if{|entry| !entry.nil?}
   end
-  def save(array)
-    open_file("w") {|file| file.puts array }
+  def update(table: '', select_column: {}, update_columns: {})
+    sql = <<~EOS
+            UPDATE #{table}
+            SET #{update_columns.map { |pair| pair.join('=') }.join(',')}
+            WHERE #{to_sql(select_column)}
+          EOS
+
+    @connection.execute(sql)
+  end
+  def table?(name)
+    sql = <<~EOS
+        SELECT count(name)
+        FROM sqlite_master
+        WHERE type='table'
+        AND name='#{name}'
+      EOS
+    @connection.execute(sql)[0][0] != 0
+  end
+  def exists?(table: '', values: {})
+    where_sql = to_sql values
+    sql = <<~EOS
+            SELECT 1
+            FROM #{table}
+            WHERE #{where_sql}
+          EOS
+    !@connection.execute(sql).empty?
+  end
+  def aggregate(log_type: '', option: '', start: nil, aggregate_function: '')
+    sql = <<~EOS
+            SELECT #{aggregate_function}(minutes)
+            FROM Logs
+            WHERE log_type=?
+            AND date>=? AND date<?
+          EOS
+
+    case option
+    when 'year'
+      @connection.execute(
+        sql,
+        [
+          log_type,
+          format_date('01-01-' + start.to_s).to_s,
+          (format_date('01-01-' + start.to_s) + 365).to_s,
+        ],
+      )
+    when 'all_time'
+      @connection.execute(
+        "SELECT #{aggregate_function}(minutes) FROM Logs WHERE log_type=?",
+        [log_type],
+      )
+    when 'month'
+      @connection.execute(
+        sql,
+        [
+          log_type,
+          format_date('01-' + start).to_s,
+          (format_date('01-' + start) + 30).to_s,
+        ],
+      )
+    else
+      @connection.execute(
+        sql,
+        [log_type, month_start.to_s, (format_date(today) + 1).to_s],
+      )
+    end[0][0] # prettier-ignore
+  end
+  def to_sql(conditions)
+    where_sql = ''
+    conditions.each { |column, value| where_sql += "#{column}='#{value}' AND " }
+    where_sql[0..-6]
+  end
+  def get_lines(filename)
+    lines = []
+    file = File.open(filename, 'r')
+    file.readlines.each { |line| lines << line unless line == "---\n" }
+    file.close
+    lines.keep_if { |entry| !entry.nil? }
+  end
+  def format_date(date)
+    Date.strptime(date, '%d-%m-%Y')
   end
   def parse_date(date)
-    DateTime.parse(Date.strptime(date, @format).to_s)
+    Date.strptime(date, @format)
   end
   def today
-    DateTime.now.strftime(@format)
+    DateTime.now.strftime('%d-%m-%Y')
   end
-  def month_start(date=DateTime.now)
+  def month_start(date = DateTime.now)
     parse_date(date.strftime('01-%m-%Y'))
   end
 end
--- spec/time_logger_spec.rb
+++ spec/time_logger_spec.rb
@@ -1,10 +1,107 @@
-require './lib/time_logger.rb'
+require './lib/time_logger'
+require 'date'
 
 RSpec.configure do |c|
   c.example_status_persistence_file_path = 'spec/examples.txt'
 end
 
 RSpec.describe 'Logger' do
-  it 'returns something' do
+  time_log = Logger.new(filename: ':memory:')
+  count_records = ->(name) { time_log.connection.execute(<<~EOS)[0][0] }
+    SELECT count(*)
+    FROM #{name}
+  EOS
+
+  has_table = ->(name) { time_log.connection.execute(<<~EOS)[0][0] != 0 }
+    SELECT count(name)
+    FROM sqlite_master
+    WHERE type='table'
+    AND name='#{name}'
+  EOS
+
+  it 'creates database' do
+    expect(time_log.connection.nil?).to eq(false)
+  end
+  it 'creates Log and LogTypes tables' do
+    expect(has_table.('Logs')).to eq(true)
+    expect(has_table.('LogTypes')).to eq(true)
+  end
+  it 'creates Log records and appropriate LogType entry' do
+    time_log.add_or_update(log_type: 'dog_walking', mins: 44)
+    expect(count_records.('Logs')).to eq(1)
+    expect(count_records.('LogTypes')).to eq(1)
+    expect(
+      time_log.connection.execute('SELECT minutes FROM Logs LIMIT 1')[0][0],
+    ).to eq (44)
+  end
+  it 'does not create duplicate LogTypes or Logs rows' do
+    time_log.add_or_update(log_type: 'dog_walking', mins: 44)
+    time_log.add_or_update(log_type: 'dog_walking', mins: 44)
+    expect(count_records.('Logs')).to eq(1)
+    expect(
+      time_log.connection.execute('SELECT minutes FROM Logs LIMIT 1')[0][0],
+    ).to eq (44 * 3)
+    expect(count_records.('LogTypes')).to eq(1)
+  end
+  it 'deletes Log records' do
+    time_log.add_or_update(log_type: 'Gaming', mins: 101)
+
+    log_quantity = count_records.('Logs')
+    log_type_quantity = count_records.('LogTypes')
+
+    time_log.delete(table: 'Logs', remove: { log_type: 'Gaming' })
+    time_log.delete(table: 'LogTypes', remove: { name: 'Gaming' })
+
+    expect(count_records.('Logs')).to be < log_quantity
+    expect(count_records.('LogTypes')).to be < log_type_quantity
+  end
+  it 'displays all records' do
+    time_log.add_or_update(log_type: 'dog_walking', mins: 44, date: '01-01-01')
+    time_log.add_or_update(log_type: 'dog_walking', mins: 44, date: '02-02-02')
+    time_log.add_or_update(log_type: 'dog_walking', mins: 44, date: '03-03-03')
+
+    expect(time_log.all(display: 'dog_walking', width: 20, row_size: 3)).to eq(
+      ['01-01-01: 44', '02-02-02: 44', '03-03-03: 44'].map { |text|
+        text.center(20)
+      }.join(' | ') +
+        "\n#{"#{DateTime.now.strftime('%d-%m-%y')}: 132".center(20)} | ",
+    )
+
+    time_log.add_or_update(log_type: 'Gaming', mins: 45, date: '03-03-03')
+
+    expect(time_log.all(display: 'LogTypes', width: 40, row_size: 1)).to eq(
+      %w[dog_walking Gaming].map { |text| text.center(40) + "\n" }.join(''),
+    )
+  end
+  it 'returns sum of all records with X log_type' do
+    expect(time_log.sum(log_type: 'dog_walking')).to eq(44 * 3)
+    expect(
+      time_log.sum(log_type: 'dog_walking', option: 'month', start: '01-01'),
+    ).to eq(44)
+    expect(
+      time_log.sum(log_type: 'dog_walking', option: 'year', start: 03),
+    ).to eq(44)
+    expect(time_log.sum(log_type: 'dog_walking', option: 'all_time')).to eq(
+      44 * 6,
+    )
+  end
+  it 'returns average of all records with X log_type' do
+    time_log.add_or_update(log_type: 'dog_walking', mins: 44, date: '01-01-01')
+    time_log.add_or_update(log_type: 'dog_walking', mins: 44, date: '01-01-01')
+    expect(
+      time_log.average(log_type: 'dog_walking', option: 'year', start: '01'),
+    ).to eq(44.0 * 3)
+  end
+  it 'imports entries from YAML' do
+    quantity = count_records.('Logs')
+    time_log.import_from_yaml(log_type: 'Maille')
+    expect(count_records.('Logs')).to eq (quantity + 3)
+    expect(time_log.all(display: 'Maille', width: 20, row_size: 3)).to eq(
+      (
+        ['01-01-21: 10', '08-05-21: 10'].map { |text| text.center(20) } + [
+          '09-05-21: 297'.center(20) + "\n",
+        ]
+      ).join(' | '),
+    )
   end
 end
